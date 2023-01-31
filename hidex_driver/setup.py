a57dbf4e262ab0138cf83ef1c890c8c43685cffb
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-01-31 11:54:18 -0600

    init

--- hidex_driver/setup.py
+++ hidex_driver/setup.py
@@ -2,17 +2,12 @@ import os
 from setuptools import setup, find_packages
 
 
-package_name = 'hidex_driver'
 
 setup(
     name='hidex_driver',
     version='0.0.1',
     packages=find_packages(),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=[],
     install_requires=[],
     zip_safe=True,
     python_requires=">=3.8",
@@ -21,7 +16,5 @@ setup(
     description='',
     url='', 
     license='MIT License',
-    entry_points={ 
-        'console_scripts': []
-    },
+    entry_points={},
 )
