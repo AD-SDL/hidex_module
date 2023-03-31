bdb1a2bcb21c51f4a73da82b5fc39b9533041d90
Casey Stone <cstone@anl.gov>
2023-03-31 14:46:41 -0500

    hidex ahks working but need more testing

--- hidex_driver/ahk/hidex_open_close.py
+++ hidex_driver/ahk/hidex_open_close.py
@@ -6,11 +6,12 @@ from ahk import AHK
 from ahk.window import Window
 
 
-# Python script to run a Hidex protocol
-def hidexRun(protocol_path): 
-
+def hidexOpenClose(): 
+    """AHK script to open or close the Hidex drawer. NOTE: always start any protocol without the Hidex app open"""
+ 
     ahk = AHK()
     
+    # TODO: could check system to see if Hidex program is already running
     # open the Hidex app
     os.system('start C:\hidex\PlateReaderSoftware_Automation_1.3.1-rc\PlateReaderSoftware.exe') 
     time.sleep(5) # wait for window to open fully
@@ -35,39 +36,15 @@ def hidexRun(protocol_path):
         except Exception as error_msg: 
             print("No initial pop up window found")
 
-    # move mouse over import assay template button
-    ahk.mouse_move(2085, 329)
-    ahk.click()
-
-    time.sleep(1)
-   
     try: 
-        # locate the window
-        import_template_pop_up = ahk.find_window(title=b'Import Template From File')
-
-        if import_template_pop_up:
-        # type in the path to template
-            ahk.type(protocol_path)
-
-            ahk.key_press('Enter')
-
-    except Exception as error_msg: 
+        # look for the open close button on the screen
+        print(ahk.image_search('C:\\Users\\svcaibio\\Dev\\hidex_module\\hidex_driver\\ahk\\images\\drawer_control.jpg'))
+    
+    except Exception as error_msg:
         print(error_msg)
 
-    # run the imported protocol template file
-    ahk.mouse_move(2170,1354)
-    ahk.click()
-
-    # wait for the protocol to finish
-    while (ahk.pixel_get_color(2324,1362)) == '0x000000': 
-        print("Hidex protocol is still running")
-        print(ahk.pixel_get_color(2324,1362))
-        time.sleep(1)
-    
-    # return to main assay screen once complete 
-    print("Hidex protocol complete, returning to main assay screen")
-    time.sleep(.5)
-    ahk.mouse_move(36,168)
+    # move mouse over import assay template button
+    ahk.mouse_move(2470, 81)
     ahk.click()
 
     # minimize the hidex app after protcol complete
@@ -75,4 +52,4 @@ def hidexRun(protocol_path):
     main_hidex_window = Window.from_mouse_position(ahk)
     main_hidex_window.minimize()
 
-hidexRun("C:\\Users\\svcaibio\\Documents\\Hidex Sense\\Campaign1_noIncubate2.sensetemplate")
+hidexOpenClose()
