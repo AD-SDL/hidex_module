2788b3260bbc8047505867210c42c1559f479932
Casey Stone <cstone@anl.gov>
2023-05-01 13:32:02 -0500

    file monitoring added to hidex ahk, not tested

--- hidex_driver/ahk/hidex_auto_run.py
+++ hidex_driver/ahk/hidex_auto_run.py
@@ -13,11 +13,71 @@ def hidexRun(protocol_path):
 
     # capture contents of hidex data folder before run
     before_run__data_contents = list_data_files() 
-    is_complete = False
     action_log = ""
 
-    # run the Hidex Protocol with AHK python script ------------------------
+    # run Hidex protocol in app with ahk python script 
+    ahk_complete, ahk_log = run_protocol_ahk(protocol_path)
+    action_log += ahk_log
+
+    if ahk_complete == True: 
+        # capture contents of hidex data folder after the run 
+        after_run_data_contents = list_data_files()
+
+        # find data file added from this run 
+        new_data_files = [f for f in after_run_data_contents if f not in before_run__data_contents]
+
+        # ensure only one new data file found
+        if len(new_data_files) == 1:  
+
+            # move the file to proc folder 
+            relocated_data_path = archive_data(new_data_files[0])
+
+            # format response message variables
+            action_response = 0
+            action_msg = relocated_data_path   
+
+            # record in log
+            action_log += (f"({datetime.now()}) CLIENT: one new hidex data file found, {relocated_data_path}\n")
+
+        else: # if there are 0 or more than one new data file
+
+            # TODO: pick the most recent file here? means something went wrong before this run
+            
+            #  format response message variables
+            action_response = -1
+            action_msg = []
+            
+            # record in log
+            action_log += (f"({datetime.now()}) ERROR CLIENT: {len(new_data_files)} hidex data files found, should be 1.\n")
+            
+    else: # if ahk did not complete 
+
+        # format response message variables 
+        action_response = -1
+        action_msg = []
+
+        # record in log
+        action_log += (f"({datetime.now()}) ERROR CLIENT: Hidex ahk sequence did not complete.\n")
+
+    # format return dict
+    return_dict = {
+        'action_response': action_response,
+        'action_msg': action_msg,
+        'action_log': action_log
+    }
+
+    return return_dict 
+
+
+# HELPER METHODS -----------------------------------------------
+
+def run_protocol_ahk(protocol): 
+    """
+    
+    """
     ahk = AHK()
+    ahk_log = ""
+    ahk_complete = False 
     
     # open the Hidex app
     os.system('start C:\hidex\PlateReaderSoftware_Automation_1.3.1-rc\PlateReaderSoftware.exe') 
@@ -35,13 +95,13 @@ def hidexRun(protocol_path):
 
                 # wait for Hidex to initialize
                 while ahk.pixel_get_color(1824,76) != '0xFFFFFF':
-                    print(f"({datetime.now()})  AHK: Waiting for hidex to initialize") 
+                    ahk_log += (f"({datetime.now()})  AHK: Waiting for hidex to initialize\n") 
                     time.sleep(1)
                 time.sleep(.5)
 
         except Exception as error_msg: 
-            print(f"({datetime.now()}) AHK ERROR: hidexRun unable to close initial pop up window")
-            print(error_msg)
+            ahk_log += (f"({datetime.now()}) AHK ERROR: hidexRun unable to close initial pop up window\n")
+            ahk_log += print(error_msg) + "\n"
 
     # move mouse over import assay template button
     ahk.click(1765,318)
@@ -53,83 +113,41 @@ def hidexRun(protocol_path):
 
         if import_template_pop_up:
         # type in the path to template
-            ahk.type(protocol_path)
+            ahk.type(protocol)
 
             ahk.key_press('Enter')
 
     except Exception as error_msg: 
-        print(error_msg)
+        ahk_log += error_msg + "\n"
 
     # run the imported protocol template file
     ahk.click(1700,990)
 
     # wait for the protocol to finish
     while (ahk.pixel_get_color(1700,990)) == '0x000000':  # check that run now button pixels are black (green when run complete)
-        print(f"({datetime.now()}) AHK: Hidex protocol is still running")
+        ahk_log += (f"({datetime.now()}) AHK: Hidex protocol is still running\n")
         time.sleep(1)
     
     # return to main assay screen once complete 
-    print(f"({datetime.now()}) AHK: Hidex protocol complete, returning to main assay screen")
+    ahk_log += (f"({datetime.now()}) AHK: Hidex protocol complete, returning to main assay screen\n")
     time.sleep(.5)
     ahk.click(36,168)
 
-    # minimize the hidex app after protcol complete
+    # minimize the hidex app after protocol complete
     time.sleep(.5) 
-    main_hidex_window = Window.from_mouse_position(ahk)
-    main_hidex_window.minimize()
+    try: 
+        main_hidex_window = Window.from_mouse_position(ahk)
+        main_hidex_window.minimize()
+        ahk_complete = True
+
+    except Exception as error_msg: 
+        ahk_log += (f"({datetime.now()}) AHK ERROR: could not minimize main hidex window\n")
+        ahk_log += error_msg + "\n"
+
+    return ahk_complete, ahk_log
     
-    # end of ahk python actions -----------------------------------------
-    
-    
-    # capture contents of hidex data folder after the run 
-    after_run_data_contents = list_data_files()
-
-    # find data file added from this run 
-    new_data_files = [f for f in after_run_data_contents if f not in before_run__data_contents]
-
-    # make sure one data file captured, not > 1 or 0
-    if len(new_data_files) == 1: 
-        
-        # TESTING
-        print("one data file found")
-        print(new_data_files) 
-
-        new_data_path = new_data_files[0]
-        
-        action_response = 0
-        action_msg = new_data_files[0]
-
-    else: 
-        
-        # TESTING
-        print("more than one data file found!")
-        print(new_data_files)
-        action_response = -1
-        action_msg = new_data_files
-        
-
-#   # format return dict 
-#     return_dict = {
-#         'action_response': action_response,
-#         'action_msg': action_msg,
-#             'action_log': action_log,
-#         }  
-
-    is_complete = True
-    print(f"({datetime.now()}) AHK: Returning True, hidex run completed")
-
-    return is_complete
-    # TODO: also return every message in one single string
-    # TODO: action response  = 0 or 1
-    # return_dict = {
-    #     'action_response': is_complete, #int16 --> 1 if complete and some other number if an error maybe
-    #     'action_msg': something_meaninful, (filename of new data)
-    #     'action_log': string_of_logs
-    # }
-#hidexRun("C:\\Users\\svcaibio\\Documents\\Hidex Sense\\Campaign1_noIncubate2.sensetemplate")
 
 
-# HELPER METHODS -----------------------------------------------
 def list_data_files(hidex_data_folder = "C://labautomation//data_wei//one_file") -> list: 
 
     try: 
@@ -155,7 +173,7 @@ def archive_data(new_file_path, archive_folder = "C:\\labautomation\\data_wei\\p
         file_name = os.path.basename(new_file_path)
 
         if os.isdir(archive_folder):
-            archive_path = os.path.join(archive_folder, new_file_path)
+            archive_path = os.path.join(archive_folder, file_name)
 
             shutil.move(new_file_path, archive_path)
             
