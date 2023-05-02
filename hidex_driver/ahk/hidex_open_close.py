c8fe61705b7a564d7d8ee0086e43ffcc5fa32486
Casey Stone <cstone@anl.gov>
2023-05-02 15:21:11 -0500

    clean up hidex listener

--- hidex_driver/ahk/hidex_open_close.py
+++ hidex_driver/ahk/hidex_open_close.py
@@ -1,18 +1,17 @@
 import sys
 import os
 import time
+from datetime import datetime
 
 from ahk import AHK
 from ahk.window import Window
 
-
 def hidexOpenClose(): 
     """AHK script to open or close the Hidex drawer. NOTE: always start any protocol without the Hidex app open"""
-    
-    is_complete = False
+
+    action_log = ""
     ahk = AHK()
     
-    # TODO: could check system to see if Hidex program is already running
     # open the Hidex app
     os.system('start C:\hidex\PlateReaderSoftware_Automation_1.3.1-rc\PlateReaderSoftware.exe') 
     time.sleep(5) # wait for window to open fully
@@ -29,25 +28,18 @@ def hidexOpenClose():
 
                 # wait for Hidex to initialize
                 while ahk.pixel_get_color(1824,76) != '0xFFFFFF': 
-                    print("Waiting for hidex to initialize") 
+                    action_log += (f"{datetime.now()} AHK: Waiting for hidex to initialize\n") 
                     time.sleep(1)
                 time.sleep(.5)
 
         except Exception as error_msg: 
-            print("No initial pop up window found")
-
-    try: 
-        # look for the open close button on the screen
-        print(ahk.image_search('C:\\Users\\svcaibio\\Dev\\hidex_module\\hidex_driver\\ahk\\images\\drawer_control.jpg'))
-    
-    except Exception as error_msg:
-        print(error_msg)
+            action_log += (f"{datetime.now()} AHK: No initial pop up window found\n")
+            action_log += (f"{datetime.now()} AHK: {error_msg}\n")
 
     # click open/close door button
     ahk.click(1824,76) 
 
     # return to main assay screen once complete 
-    print("Action complete: returning to main assay screen")
     time.sleep(.5)
     ahk.click(36,168) 
 
@@ -56,8 +48,14 @@ def hidexOpenClose():
     main_hidex_window = Window.from_mouse_position(ahk)
     main_hidex_window.minimize()
 
-    is_complete = True 
-    print("hidex opened/closed, returning True")
+    action_response = 0
+    action_log += (f"{datetime.now()} AHK: Action complete, hidex opened/closed\n")
 
-    return is_complete
+    # format return_dict
+    return_dict = {
+        'action_response': action_response,
+        'action_log': action_log,
+    }
+
+    return return_dict
 
--- hidex_module_client/hidex_module_client/hudson01_listen_hidex.py
+++ hidex_module_client/hidex_module_client/hudson01_listen_hidex.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import zmq
+from datetime import datetime
 
 # TODO: find a better way to access the driver functions
 sys.path.append('C:\\Users\\svcaibio\\Dev\\hidex_module')
@@ -14,56 +15,73 @@ print("hudson01 listening on port 5556")
 while True:
     message = socket.recv()
     response = ""
+
     if message == b"SHUTDOWN":
         socket.send(b"Shutting down")
         break
+    
     else:
         msg = eval(message.decode("utf-8"))
 
-        #check if msg is dictionary
-        action_handle = msg['action_handle'] 
-        action_vars = msg['action_vars']
-
-        # drawer control actions
-        if action_handle=="open": 
-            print('opening hidex drawer')
-            try: 
-                is_complete = hidex_open_close.hidexOpenClose()
-                if is_complete == True: 
-                    response = "Hidex opened"
-                else: 
-                    response = "ERROR: Hidex ahk unable to open drawer"
-            except Exception as error_msg: 
-                print("ERROR: Hidex client unable to run open_close.ahk")
-
-        elif action_handle=="close": 
-            print('closing hidex drawer')
-            try: 
-                is_complete = hidex_open_close.hidexOpenClose()
-                if is_complete == True: 
-                    response = "Hidex closed"
-                else: 
-                    response = "ERROR: Hidex ahk unable to close drawer"
-            except Exception as error_msg: 
-                print("ERROR: Hidex client unable to run open_close.ahk")
-
-        # protocol handling actions
-        if action_handle=='run_protocol':
-            print('running hidex')
+        try:  # msg is formatted correctly
             
-            try: 
-                # check that action_vars is a string filename and that it exists
-                if isinstance(action_vars['protocol_path'], str) and os.path.exists(action_vars['protocol_path']): 
-                    # TESTING 
-                    print(action_vars['protocol_path'])
-                    return_dict = hidex_auto_run.hidexRun(action_vars['protocol_path'])
-                    response = str(return_dict)  # this is the right form
+            #check if msg is dictionary
+            action_handle = msg['action_handle'] 
+            action_vars = msg['action_vars']
 
-            except Exception as error_msg: 
-                print(error_msg)
-                response = "ERROR: Hidex client could not run ahk"
+            # drawer control actions
+            if action_handle=="open": 
+
+                try: 
+                    return_dict = hidex_open_close.hidexOpenClose()
+                    return_dict['action_log'] += (f"{datetime.now()} LISTEN HIDEX: Hidex Opened\n")
+                    response = str(return_dict)
+
+                except Exception as error_msg: 
+                    return_dict = {
+                        'action_response': -1,
+                        'action_log': (f"{datetime.now()} ERROR LISTEN HIDEX: Hidex client unable to run open_close.ahk\n{error_msg}")
+                    }
+                    response = str(return_dict)
+
+            elif action_handle=="close":  # same as open for now
+
+                try: 
+                    return_dict = hidex_open_close.hidexOpenClose()
+                    return_dict['action_log'] += (f"{datetime.now()} LISTEN HIDEX: Hidex Closed\n")
+                    response = str(return_dict)
+
+                except Exception as error_msg: 
+                    return_dict = {
+                        'action_response': -1,
+                        'action_log': (f"{datetime.now()} ERROR LISTEN HIDEX: Hidex client unable to run open_close.ahk\n{error_msg}")
+                    }
+                    response = str(return_dict)
+
+            # protocol handling actions
+            if action_handle=='run_protocol':
+                
+                try: 
+                    # check that action_vars is a string filename and that it exists
+                    if isinstance(action_vars['protocol_path'], str) and os.path.exists(action_vars['protocol_path']): 
+                        return_dict = hidex_auto_run.hidexRun(action_vars['protocol_path'])
+                        return_dict['action_log'] += (f"{datetime.now()} LISTEN HIDEX: Hidex Run Protocol Complete\n")
+                        response = str(return_dict) 
+
+                except Exception as error_msg: 
+                    return_dict = {
+                        'action_response': -1,
+                        'action_log': (f"{datetime.now()} ERROR LISTEN HIDEX: Hidex client could not run ahk\n{error_msg}\n")
+                    }
+                    response = str(return_dict)
         
+        except Exception as error_msg:  # msg is not formatted correctly
+            return_dict = {
+                'action_response': -1,
+                'action_log': (f"{datetime.now()} ERROR LISTEN HIDEX: Message received was not formatted correctly\n{error_msg}\n")
+            }
+            response = str(return_dict)
+
         socket.send(bytes(response, encoding='utf-8')) 
-
-        
+    
 socket.close()
