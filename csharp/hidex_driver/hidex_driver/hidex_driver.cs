bee58929a61dbb3b4b115c086232de8cd53494bb
Dozgulbas <dozgulbas@anl.gov>
2023-05-18 16:03:48 -0500

    adding gitignorestuff and pushing

--- csharp/hidex_driver/hidex_driver/hidex_driver.cs
+++ csharp/hidex_driver/hidex_driver/hidex_driver.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Project1.ServiceReference1;
-
+using System.Net.Sockets;
+using Newtonsoft.Json;
 namespace ServiceR
 {
+    public class Message
+    {
+        public string action_handle { get; set; }
+        
+        public Dictionary<string, string> action_vars { get; set; }
+    }
+
     public class test : Project1.ServiceReference1.IHidexSenseAutomationServiceCallback
     {
        public void OnStateChanged()
@@ -13,24 +21,97 @@ namespace ServiceR
             Console.WriteLine("test");
         }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            //Step 1: Create an instance of the WCF proxy.
             Project1.ServiceReference1.HidexSenseAutomationServiceClient client = new Project1.ServiceReference1.HidexSenseAutomationServiceClient(new System.ServiceModel.InstanceContext(new test()));
-            client.Connect(false);
-            Project1.ServiceReference1.InstrumentState s = client.GetState();
-            Console.WriteLine(s);
-            //client.OpenPlateCarrier() ;
-            // Step 2: Call the service operations.
-            // Call the Add service operation.
-            
+            try
+            {
+                //Step 1: Create an instance of the WCF proxy.
+               
+                client.Connect(false);
+                Project1.ServiceReference1.InstrumentState s = client.GetState();
+                Console.WriteLine(s);
+                //client.OpenPlateCarrier() ;
+                // Step 2: Call the service operations.
+                // Call the Add service operation.
 
-            // Step 3: Close the client to gracefully close the connection and clean up resources.
-            Console.WriteLine("\nPress <Enter> to terminate the client.");
-            Console.ReadLine();
+
+                // Step 3: Close the client to gracefully close the connection and clean up resources.
+                Console.WriteLine("\nPress <Enter> to terminate the client.");
+                while (s == InstrumentState.Unknown)
+                {
+                  s = client.GetState();
+                  Console.Out.WriteLine("waiting");
+                }
+
+
+
+
+
+
+                Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect("127.0.0.1", 1003);
+
+                // Send the request.
+                // For the tiny amount of data in this example, the first call to Send() will likely deliver the buffer completely,
+                // however this is not guaranteed to happen for larger real-life buffers.
+                // The best practice is to iterate until all the data is sent.
+
+                // Do minimalistic buffering assuming ASCII response
+                byte[] responseBytes = new byte[256];
+                char[] responseChars = new char[256];
+                int c = 0;
+                int bytesReceived = 0;
+                while (true)
+                {
+                    bytesReceived = socket.Receive(responseBytes);
+
+                    // Receiving 0 bytes means EOF has been reached
+                    if (bytesReceived == 0)
+                    {
+                        c = c + 1;
+                        Console.Out.Write("testasdfsf");
+                        break;
+                    }
+                    // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
+                    int charCount = Encoding.ASCII.GetChars(responseBytes, 0, bytesReceived, responseChars, 0);
+
+                    // Print the contents of the 'responseChars' buffer to Console.Out
+                    Console.Out.Write(responseChars, 0, charCount);
+                   
+                    Message m = JsonConvert.DeserializeObject<Message>(new string(responseChars));
+                    Console.Out.Write(m);
+                    Console.Out.Write(m.action_handle);
+                    Console.Out.Write(m.action_handle == "open");
+                    Console.ReadLine();
+                    if (m.action_handle == "open")
+                    {
+                        Console.ReadLine();
+                        client.OpenPlateCarrier();
+                    }
+                    if (m.action_handle == "close")
+                    {
+                        client.ClosePlateCarrier();
+                    }
+                    if (m.action_handle == "end")
+                    {
+                        break;
+                    }
+                }
+
+
+            }
+            catch(Exception e)
+            {
+                Console.Out.Write(e);
+                Console.ReadLine();
+            }
+            finally { 
             client.Close();
+            }
         }
     }
 }
---
+++ csharp/hidex_driver/hidex_driver/sockettest.py
@@ -0,0 +1,44 @@
+import socket
+import json            
+ 
+# next create a socket object
+s = socket.socket()        
+print ("Socket successfully created")
+ 
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 1003               
+ 
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+s.bind(('127.0.0.1', port))        
+print ("socket binded to %s" %(port))
+ 
+# put the socket into listening mode
+s.listen(5)    
+print ("socket is listening")           
+ 
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+ 
+# Establish connection with client.
+  c, addr = s.accept()    
+  print ('Got connection from', addr )
+  while True:
+    command = str(input())
+    # send a thank you message to the client. encoding to send byte type.
+    
+    c.send(json.dumps({"action_handle": command, "action_vars": {}
+    }).encode())
+    if command == "State":
+        test = c.recv(1024)
+        print(test)
+  # Close the connection with the client
+  c.close()
+   
+  # Breaking once connection closed
+  break
