7d3987bce997573df00305e6a0ec5177930b047f
Casey Stone <cstone@anl.gov>
2024-08-21 16:42:22 -0500

    Update to wei v0.6.0

--- src/hidex_rest_node.py
+++ src/hidex_rest_node.py
@@ -4,7 +4,7 @@
 REST-based node that interfaces with WEI and provides various fake actions for testing purposes
 """
 import clr
-clr.AddReference("C:\\Users\\rpl\\source\\repos\\hidex_module\\hidex_node\\bin\\Debug\\HidexNode.dll")
+clr.AddReference("C:\\Users\\svcaibio\\\Dev\\hidex_module\\hidex_node\\bin\\Debug\\HidexNode.dll")
 import System.ServiceModel as SM
 import System.ServiceModel.Channels as SMC
 import HidexNode.HidexAutomation as HA
@@ -15,20 +15,19 @@ import glob
 import os
 
 
-print(HA.IHidexSenseAutomationServiceCallback)
-
 from typing import Annotated
 
 from fastapi import UploadFile
 from fastapi.datastructures import State
 from wei.modules.rest_module import RESTModule
 from wei.types import StepFileResponse, StepResponse, StepStatus
+from wei.types.step_types import StepSucceeded
 from wei.types.module_types import (
     LocalFileModuleActionResult,
     Location,
     ModuleState,
     ValueModuleActionResult,
-    ModuleStatus
+    ModuleStatus,
 )
 from wei.types.step_types import ActionRequest
 
@@ -70,7 +69,6 @@ def test_node_startup(state: State):
         time.sleep(0.5)
     print(t.GetState())
     state.client = t
-    state.prev_file_name = ""
 
 
 @hidex_rest_node.state_handler()
@@ -114,25 +112,33 @@ def close(
     ],
 )
 def run_assay(state: State, action: ActionRequest,
-                   assay_name: Annotated[str, "assay to run"]) -> StepFileResponse:
+                   assay_name: Annotated[str, "assay to run"], wait_for_result: Annotated[bool, "Whether we should wait for the results of the assay before returning"] = True) -> StepFileResponse:
     """runs assay on the current sample"""
 
     list_of_files = glob.glob(state.output_path +'\\*') # * means all if need specific format then *.csv
     latest_file = max(list_of_files, key=os.path.getctime)
+    prev_file = latest_file
     print(latest_file)
     state.client.SetAutoExportPath(state.output_path)
+    state.prev
     state.client.StartAssay(assay_name)
     while state.client.GetState() == HA.InstrumentState.Busy:
         pass
-    while latest_file == state.prev_file_name:
-        list_of_files = glob.glob(state.output_path +'\\*') # * means all if need specific format then *.csv
-        latest_file = max(list_of_files, key=os.path.getctime)
-        time.sleep(0.5)
-    state.prev_file_name = latest_file
-    return StepFileResponse(
-        StepStatus.SUCCEEDED,
-        files={"assay_result": latest_file},
-    )
+    if bool(wait_for_result):
+        while latest_file == prev_file:
+            list_of_files = glob.glob(state.output_path +'\\*') # * means all if need specific format then *.csv
+            latest_file = max(list_of_files, key=os.path.getctime)
+            time.sleep(0.5)
+        return StepFileResponse(
+            StepStatus.SUCCEEDED,
+            files={"assay_result": latest_file},
+        )
+    else:
+        return StepSucceeded()
+
+@hidex_rest_node.shutdown()
+def shutdown(state: State):
+    state.client.Disconnect()
 
 
 if __name__ == "__main__":
